c4e1db704dcf9c8f371ad6ba4298d94a145ca4a0
spk-krishna567 <20191a0567jntucep@gmail.com>
2022-10-24 17:56:31 +0530

    by uma

--- includes/links.php
+++ includes/links.php
@@ -1,10 +1,14 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 <script src="https://kit.fontawesome.com/9ecb36fae2.js" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Montserrat+Alternates:wght@600&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-brands/css/uicons-brands.css'>
--- index.php
+++ index.php
@@ -1,77 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>P-RESOURCE</title>
     <link rel="stylesheet" href="assets/css/style.css">
-    
-    <?php include 'includes/links.php';?>
+
+    <?php include 'includes/links.php'; ?>
 </head>
+
 <body>
-<?php include 'includes/navbar.php';?>
+    <?php include 'includes/navbar.php'; ?>
 
-<div class=" col-md-9 offset-md-2 col-sm-12 mt-5 ">
-    <div class=" section1 ">
-    <div class="text">
-    <p class="heading1">Start learning with us</p>
-    <p class="matter">Don’t know where to start learning computer science,
-       then you are at right place .start exploring our resourses and
-        build your your skills towards your desired career and domain </p>
-        <button class="end-btn">then Go get started<i class="fa-solid fa-arrow-right"></i></button>
-     </div>
-        <img src="assets\images\image1.png" alt="image1">
-    </div>
+    <div class=" col-md-9 offset-md-1 col-sm-12 mt-5 ">
+        <div class=" section1 ">
+            <div>
+                <img src="assets\images\image1.png" alt="image1" class="pngimg">
+            </div>
+            <div class="text">
+                <p class="heading1">Start learning with us</p>
+                <p class="matter">Don’t know where to start learning computer science,
+                    then you are at right place .start exploring our resourses and
+                    build your your skills towards your desired career and domain </p>
+                <button class="end-btn">then Go get started<i class="fa-solid fa-arrow-right"></i></button>
+            </div>
 
-
-    <div class=" section2 ">
-    <img src="assets\images\image2.png" alt="image1">
-    <div class="text">
-        <p class="s_heading" style="width: 376px;height: 73px !important">we provide</p>
-        <ul class="matter"><li>breif explanation about software domains</li>
-                          <li>resources according to domains</li>
-                           <li>breif discussion about career plans in software industry</li> </ul>
         </div>
-        
-    </div>
 
 
-    <div class=" section3 ">
-    <div class="text">
-    <p class="s_heading">we suggest career as</p>
-    <ul class="matter"><li>Developer</li>
-                          <li>designer</li>
-                           <li>enterprenuer</li> </ul>
-     </div>
-        <img src="assets\images\image3.png" alt="image1">
-    </div>
+        <div class=" section2 ">
+            <img src="assets\images\image2.png" alt="image1" class="pngimg">
+            <div class="text">
+                <p class="s_heading" style="width: 376px;height: 73px !important">we provide</p>
+                <ul class="matter">
+                    <li>breif explanation about software domains</li>
+                    <li>resources according to domains</li>
+                    <li>breif discussion about career plans in software industry</li>
+                </ul>
+            </div>
 
-    <div class=" section4 ">
-        <img src="assets\images\image4.png" alt="image1">
-    <div class="text">
-    <p class="s_heading">we enhance u in</p>
-    <ul class="matter"><li>no coding platforms</li>
-                          <li>building tech product</li>
-                           <li>advance technologies</li>
-                           <li>building your skill from scratch</li> </ul>
-     </div>
-        
-    </div>
-   
-    <div class="card mt-5">
-        <div class="text-end">
-        we think u are ready
         </div>
-        <button class="end-btn">get started <i class="fa-solid fa-arrow-right"></i></button>
+
+
+        <div class=" section3 ">
+            <img src="assets\images\image3.png" alt="image1" class="pngimg">
+            <div class="text">
+                <p class="s_heading">we suggest career as</p>
+                <ul class="matter">
+                    <li>Developer</li>
+                    <li>designer</li>
+                    <li>enterprenuer</li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class=" section4 ">
+            <img src="assets\images\image4.png" alt="image1" class="pngimg">
+            <div class="text">
+                <p class="s_heading">we enhance u in</p>
+                <ul class="matter">
+                    <li>no coding platforms</li>
+                    <li>building tech product</li>
+                    <li>advance technologies</li>
+                    <li>building your skill from scratch</li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="card mt-5">
+            <div class="text-end">
+                we think u are ready
+            </div>
+            <button class="end-btn">get started <i class="fa-solid fa-arrow-right"></i></button>
+        </div>
+
     </div>
 
-</div>
- 
-    
 
 
- 
-<?php include 'includes/footer.php'; ?>
+
+
+    <?php include 'includes/footer.php'; ?>
 </body>
+
 </html>
